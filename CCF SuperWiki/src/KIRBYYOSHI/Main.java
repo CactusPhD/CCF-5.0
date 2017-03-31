ec525ec23acbad0276964bc5233a8e5a347127ca
andrewyosh <coolasy@mac.com>
2017-03-31 18:58:43 -0500

    fgfdgfd

--- CCF SuperWiki/src/KIRBYYOSHI/Main.java
+++ CCF SuperWiki/src/KIRBYYOSHI/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args){
         dataInput d = new dataInput();
         d.inputDataArt();
-        ArrayList<publicArt> alist = new ArrayList<>();
+        ArrayList<publicArt> alist = d.getArrayList();
         System.out.println(alist.get(0).zipCode);
 
     }
