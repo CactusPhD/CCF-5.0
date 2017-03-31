aec8f234ea57616642033d7417df966c01d2094b
andrewyosh <coolasy@mac.com>
2017-03-31 18:34:48 -0500

    Data Input for Art
    created data input class and method to input art spreadsheet

---
+++ CCF SuperWiki/src/KIRBYYOSHI/dataInput.java
@@ -0,0 +1,28 @@
+package KIRBYYOSHI;
+import java.util.*;
+import java.io.*;
+/**
+ * Created by coola on 3/31/2017.
+ */
+public class dataInput {
+    public void inputDataArt(){
+        Scanner input = new Scanner("Lafayette_Public_Art.txt");
+        publicArt art;
+        ArrayList<publicArt> alist = new ArrayList<publicArt>();
+        String line;
+        //Loop for data input from lafayette public art
+        while (input.hasNextLine()){
+            line = input.nextLine();
+            String[] lineArray = line.split("   ");
+            art = new publicArt(Double.parseDouble(lineArray[0]),Double.parseDouble(lineArray[1]),
+                    lineArray[3],lineArray[4],Integer.parseInt(lineArray[7]),lineArray[9],
+                    lineArray[10],lineArray[11]);
+            //adds the data member to an arraylist
+            alist.add(art);
+
+
+        }
+
+    }
+
+}
