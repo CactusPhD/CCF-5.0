635b386c7682e92a2752ea2c211912e96212c466
G4110W4Y <prrebels17chad@gmail.com>
2017-04-01 03:33:56 -0500

    RIP TAR

---
+++ CCF SuperWiki/src/KIRBYYOSHI/FixDaBusData/src/fixdabusdata/FixDaBusData.java
@@ -0,0 +1,85 @@
+
+package fixdabusdata;
+
+import java.util.*;
+import java.io.*;
+
+public class FixDaBusData {
+
+    public static void main(String[] args) {
+        int counterN = 0;
+        int counterW = 0;
+        int counterS = 0;
+        String listWBus, listSBus;
+        BusRoute[] NightBuses = new BusRoute[348];
+        BusRoute[]WeekBuses = new BusRoute[700];
+        try {
+            File WBus = new File("WeekDayBus.txt");
+            File SBus = new File("SatBus.txt");
+            File NBus = new File("NightRoutes.txt");
+            Scanner inputW = new Scanner(WBus);
+            Scanner inputS = new Scanner(SBus);
+            Scanner inputN = new Scanner(NBus);
+            
+            while(inputW.hasNext()){
+              String x = inputW.nextLine();
+              String[] list = x.split(",");
+              String s0 = list[0];
+              String s1 = list[1];
+              String s2 = list[2];
+              String s3 = list[3];
+              String StopNW = list[5];
+              int BusNumW = Integer.parseInt(s0);
+              int StopNumW = Integer.parseInt(s1);
+              double xCor = Double.parseDouble(s2);
+              double yCor = Double.parseDouble(s3);
+              BusRoute DaBus = new BusRoute(BusNumW , StopNumW , xCor , yCor, StopNW);
+              System.out.println("StopNW = "+ DaBus.getStopName());
+              counterW++;
+              System.out.println(counterW);
+            }
+           
+            while(inputS.hasNext()){
+                String s = inputS.nextLine();
+                System.out.println(s);
+                String[] list = s.split(",");
+                String s0 = list[0];
+                String s1 = list[1];
+                String s2 = list[2];
+                String s3 = list[3];
+                String StopNS = list[5];
+                int BusNumS = Integer.parseInt(s0);
+              int StopNumS = Integer.parseInt(s1);
+              double xCor = Double.parseDouble(s2);
+              double yCor = Double.parseDouble(s3);
+              BusRoute DaBus = new BusRoute(BusNumS , StopNumS , xCor , yCor, StopNS);
+              System.out.println("StopNS = "+ DaBus.getStopName());
+              counterS++;
+              System.out.println(counterS);
+            }
+            
+            
+           while(inputN.hasNext()){
+               String s = inputN.nextLine();
+               String[]  list = s.split(",");
+               String s0 = list[0];
+               String s1 = list[1];
+               String s2 = list[2];
+               String s3 = list[3];
+               String StopNN = list[5];
+               int BusNumN = Integer.parseInt(s0);
+               int StopNumN = Integer.parseInt(s1);
+               double xCor = Double.parseDouble(s2);
+               double yCor = Double.parseDouble(s3);
+               BusRoute DaBus = new BusRoute(BusNumN , StopNumN , xCor , yCor, StopNN);
+               NightBuses[counterN] = DaBus;
+               counterN++;
+           }
+           
+        }catch(Exception e){
+
+        }
+        System.out.println("night bus num = "+ NightBuses[340].getBusNum());
+    }
+    
+}
