32ec2e779f3e6e79527db45c6cd33f7a029ead93
pixelrazor <darkria45@gmail.com>
2017-03-31 21:24:14 -0500

    STuff

--- CCF SuperWiki/src/ccf/superwiki/CCFSuperWiki.java
+++ CCF SuperWiki/src/ccf/superwiki/CCFSuperWiki.java
@@ -18,7 +18,7 @@ public class CCFSuperWiki extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Home.fxml"));
         
         Scene scene = new Scene(root);
         
